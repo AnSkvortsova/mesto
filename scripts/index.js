37f3b4a77fb93fe65fbb33bf7a391592b9e78c4c
Anastasiya Skvortsova <skvortsova.ak@gmail.com>
2021-05-04 11:46:32 +0300

    done

--- scripts/index.js
+++ scripts/index.js
@@ -1,60 +1,3 @@
-// переменные открытия и закрытия popup
-const openPopupEditButton = document.querySelector('.profile__edit-button');
-const openPopupAddButton = document.querySelector('.profile__add-button');
-const popupEdit = document.querySelector('.popup__edit');
-const popupAdd = document.querySelector('.popup__add');
-const closePopupEditButton = document.querySelector('#popup__edit-close');
-const closePopupAddButton = document.querySelector('#popup__add-close');
-
-const formEdit = document.querySelector('#popup__edit-container');
-const formAdd = document.querySelector('#popup__add-container');
-
-// переменные для работы с input попапа редактировать
-const title = document.querySelector('.profile__title');
-const subtitle = document.querySelector('.profile__subtitle');
-const nameInput = document.querySelector('#input-name');
-const jobInput = document.querySelector('#input-job');
-
-// переменная куда добовлять карточки
-const elementsContainer = document.querySelector('.elements');
-
-// переменные для работы с template
-const cardTemplate = document.querySelector('#card-template');
-const elementTemplate = cardTemplate.content.querySelector('.element');
-const inputPlace = document.querySelector('#input-place');
-const inputLink = document.querySelector('#input-link');
-
-
-
-// функции открытия и закрытия popup
-
-function openPopupEdit() {
-  popupEdit.classList.add('popup_opend');
-  nameInput.value = title.textContent;
-  jobInput.value = subtitle.textContent;
-}
-
-function openPopupAdd() {
-  popupAdd.classList.add('popup_opend');
-  nameInput.value = title.textContent;
-  jobInput.value = subtitle.textContent;
-}
-
-function closePopupEdit() {
-  popupEdit.classList.remove('popup_opend');
-}
-
-function closePopupAdd() {
-  popupAdd.classList.remove('popup_opend');
-}
-
-// вызов функции открытия и закрытия popup
-
-openPopupEditButton.addEventListener('click', openPopupEdit);
-openPopupAddButton.addEventListener('click', openPopupAdd);
-closePopupEditButton.addEventListener('click', closePopupEdit);
-closePopupAddButton.addEventListener('click', closePopupAdd);
-
 // массив карточек
 const initialCards = [
   {
@@ -83,6 +26,86 @@ const initialCards = [
   }
 ]; 
 
+
+
+// переменные открытия и закрытия popup
+const openPopupEditButton = document.querySelector('.profile__edit-button');
+const openPopupAddButton = document.querySelector('.profile__add-button');
+const popupEdit = document.querySelector('.popup__edit');
+const popupAdd = document.querySelector('.popup__add');
+const popupImage = document.querySelector('.popup__image');
+const closePopupEditButton = document.querySelector('#popup__edit-close');
+const closePopupAddButton = document.querySelector('#popup__add-close');
+const closePopupImageButton = document.querySelector('#popup__img-close');
+
+// переменные для работы с формой
+const formEdit = document.querySelector('#popup__edit-container');
+const formAdd = document.querySelector('#popup__add-container');
+
+// переменные для работы с input попапа редактировать
+const title = document.querySelector('.profile__title');
+const subtitle = document.querySelector('.profile__subtitle');
+const nameInput = document.querySelector('#input-name');
+const jobInput = document.querySelector('#input-job');
+
+// переменные для работы с input попапа добавить
+const inputPlace = document.querySelector('#input-place');
+const inputLink = document.querySelector('#input-link');
+
+// переменные для popap-image
+const newLinkPopup = document.querySelector('.popup__image-big');
+const newPlacePopup = document.querySelector('.popup__text');
+
+// переменные для работы с template
+const cardTemplate = document.querySelector('#card-template');
+const elementTemplate = cardTemplate.content.querySelector('.element');
+
+// переменная куда добовлять карточки
+const elementsContainer = document.querySelector('.elements');
+
+
+
+
+// функции открытия и закрытия popup 
+
+function openPopupEdit() {
+  popupEdit.classList.add('popup_opend');
+  nameInput.value = title.textContent; 
+  jobInput.value = subtitle.textContent;
+}
+
+function openPopupAdd() {
+  popupAdd.classList.add('popup_opend');
+}
+
+function openPopupImage(cardPlace, cardLink) {
+  popupImage.classList.add('popup_opend');
+  newLinkPopup.src = cardLink;
+  newPlacePopup.textContent = cardPlace;
+}
+
+function closePopupEdit() {
+  popupEdit.classList.remove('popup_opend');
+}
+
+function closePopupAdd() {
+  popupAdd.classList.remove('popup_opend');
+}
+
+function closePopupImage() {
+  popupImage.classList.remove('popup_opend');
+}
+
+// вызов функции открытия и закрытия popup 
+
+openPopupEditButton.addEventListener('click', openPopupEdit);
+openPopupAddButton.addEventListener('click', openPopupAdd);
+closePopupEditButton.addEventListener('click', closePopupEdit);
+closePopupAddButton.addEventListener('click', closePopupAdd);
+closePopupImageButton.addEventListener('click', closePopupImage);
+
+
+
 // создание карточки
 function createCard(cardPlace, cardLink) {
   const newElement = elementTemplate.cloneNode(true);
@@ -93,20 +116,24 @@ function createCard(cardPlace, cardLink) {
   const likeCardButton = newElement.querySelector('.element__heart');
 
   newLink.src = cardLink;
-  newPlace.innerText = cardPlace;
+  newPlace.textContent = cardPlace;
+
+  // вызов функции открытия popup-image
+  newLink.addEventListener('click', function(){
+    openPopupImage(cardPlace, cardLink);
+  })
 
   // вызов функции удалить карточку
-  function removeCard(e) {
-    e.target.closest('.element').remove();
+  function removeCard(evt) {
+    evt.target.closest('.element').remove();
   }
   removeCardButton.addEventListener('click', removeCard);
+
   // вызов функции нравится карточка
-  function likeCard(e) {
-    e.target.classList.toggle('element__heart_active');
+  function likeCard(evt) {
+    evt.target.classList.toggle('element__heart_active');
   }
-
   likeCardButton.addEventListener('click', likeCard);
-
   
   return newElement;
 }
